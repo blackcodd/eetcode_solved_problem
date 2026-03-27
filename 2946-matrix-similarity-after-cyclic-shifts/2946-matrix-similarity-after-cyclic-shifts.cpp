d6adee312cef119ceeb1062ba92da8ed0a8b1cfb
S_DAS <158051992+blackcodd@users.noreply.github.com>
2026-03-27 11:31:55 +0600

    Time: 3 ms (30.43%), Space: 32.7 MB (18.63%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    void left_shift(vector<int>& v) {
+        int sz = v.size();
+        int f = v[0];
+        for (int i = 1; i < sz; i++) {
+            v[i - 1] = v[i];
+        }
+        v[sz - 1] = f;
+    };
+    void right_shift(vector<int>& v) {
+        int sz = v.size();
+        int l = v[sz - 1];
+        for (int i = sz - 2; i >= 0; i--) {
+            v[i + 1] = v[i];
+        }
+        v[0] = l;
+    };
+
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        vector<vector<int>> prev = mat;
+        int m = mat.size();
+        int n = mat[0].size();
+        int shift = k% n;
+        if (shift == 0)
+            return true;
+        while (shift) {
+
+            for (int i = 0; i < m; i++) {
+
+                if (i % 2 == 0) {
+                    left_shift(mat[i]);
+                } else {
+                    right_shift(mat[i]);
+                }
+            }
+            
+            shift--;
+
+        }
+for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (mat[i][j] != prev[i][j])
+                return false;
+        }
+    }
+    return true;
+        
+    }
+
+}
+;
