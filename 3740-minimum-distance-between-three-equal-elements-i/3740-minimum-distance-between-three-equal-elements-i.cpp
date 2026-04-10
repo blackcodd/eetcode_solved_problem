231e81c73f29e002fa2c6ad4db6f94786558d8b0
S_DAS <158051992+blackcodd@users.noreply.github.com>
2026-04-10 14:13:49 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -6,6 +6,7 @@ public:
      int ans=1000;
        for(int i=0;i<nums.size();i++){
         for(int j=i+1;j<nums.size();j++){
+            if(nums[i]!=nums[j]) continue;
             for(int k=j+1;k<nums.size();k++){
 
                             if(nums[i]==nums[j] && nums[j]==nums[k]){
