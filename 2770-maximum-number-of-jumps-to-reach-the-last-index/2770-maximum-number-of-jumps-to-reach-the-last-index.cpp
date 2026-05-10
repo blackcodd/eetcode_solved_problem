05b34259487e02f3db7ed6f5c653b5a75a0afc3d
S_DAS <158051992+blackcodd@users.noreply.github.com>
2026-05-10 23:09:27 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,38 +1,26 @@
 class Solution {
-private:
-
-    int solve(vector<int>& nums, int ind, int target, vector<int>& dp) {
-
-        if(ind == 0)
-            return 0;
-
-        if(dp[ind] != -2)
-            return dp[ind];
-
-        int ans = -1;
-
-        for(int i = ind - 1; i >= 0; i--) {
-
-            if(abs(nums[ind] - nums[i]) <= target) {
-
-                int temp = solve(nums, i, target, dp);
-
-                if(temp != -1)
-                    ans = max(ans, temp + 1);
-            }
-        }
-
-        return dp[ind] = ans;
-    }
-
 public:
-
     int maximumJumps(vector<int>& nums, int target) {
 
         int n = nums.size();
 
-        vector<int> dp(n, -2);
+        vector<int> dp(n, -1);
 
-        return solve(nums, n - 1, target, dp);
+        dp[0] = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            if(dp[i] == -1) continue;
+
+            for(int j = i + 1; j < n; j++) {
+
+                if(abs(nums[j] - nums[i]) <= target) {
+
+                    dp[j] = max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
     }
 };
