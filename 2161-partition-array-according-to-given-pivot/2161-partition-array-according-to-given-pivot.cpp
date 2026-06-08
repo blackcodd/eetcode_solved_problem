49ea6e353d65ef72e503069597c30714d974c565
S_DAS <158051992+blackcodd@users.noreply.github.com>
2026-06-09 00:53:19 +0600

    Time: 223 ms (5.25%), Space: 127.6 MB (98.06%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,23 +2,24 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-
-        vector<int> less, equal, greater;
-        for (int num : nums) {
-            if (num < pivot) {
-                less.push_back(num);
-            } else if (num == pivot) {
-                equal.push_back(num);
-            } else {
-                greater.push_back(num);
+        // In-place stable partition: first move all < pivot to front,
+        // then move all == pivot after them. Uses std::rotate to keep
+        // relative order and works in-place (O(n^2) worst-case).
+        auto stable_partition_range = [&](int l, int r, auto pred) {
+            // partition [l, r) so that elements satisfying pred come first
+            int write = l;
+            for (int read = l; read < r; ++read) {
+                if (pred(nums[read])) {
+                    if (read != write) std::rotate(nums.begin() + write, nums.begin() + read, nums.begin() + read + 1);
+                    ++write;
+                }
             }
-        }
-        vector<int> result;
-        result.reserve(nums.size());
-        result.insert(result.end(), less.begin(), less.end());
-        result.insert(result.end(), equal.begin(), equal.end());
-        result.insert(result.end(), greater.begin(), greater.end());
-        return result;
-        
-    }
+            return write; // first index after moved block
+        };
+
+        int n = nums.size();
+        int mid = stable_partition_range(0, n, [&](int x){ return x < pivot; });
+        stable_partition_range(mid, n, [&](int x){ return x == pivot; });
+        return nums;
+    }       
 };
